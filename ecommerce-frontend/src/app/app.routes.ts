14e18d053a9639c605bc2ca3eb98ca309f0c7856
Garik <garikcareer@gmail.com>
2024-05-12 22:32:05 -0400

    Added model and service for product. Added routes for app.
    Moved from core. No need core for now.

--- ecommerce-frontend/src/app/app.routes.ts
+++ ecommerce-frontend/src/app/app.routes.ts
@@ -1,3 +1,13 @@
-import { Routes } from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
+import {NgModule} from "@angular/core";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'ecommerce', pathMatch: 'full'}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+
---
+++ ecommerce-frontend/src/app/models/product.model.ts
@@ -0,0 +1,6 @@
+export class ProductModel {
+  id?: any;
+  price?: string;
+  description?: string;
+  published?: boolean;
+}
