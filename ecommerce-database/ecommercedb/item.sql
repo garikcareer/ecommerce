2ac8e119135487bd43f650df3fabbbe5f38932b0
Garik <garikcareer@gmail.com>
2024-07-21 09:52:07 -0700

    Gradle refactoring, versions are managed from gradle.properties and plugins as well. Simpler.

--- ecommerce-database/ecommercedb/item.sql
+++ ecommerce-database/ecommercedb/item.sql
@@ -1,8 +1,8 @@
 create table ecommercedb.item
 (
     item_id       bigint unsigned as (null) stored,
-    item_title    varchar(255) not null,
-    item_subtitle varchar(255) null,
+    item_name    varchar(255) not null,
+    item_description varchar(255) null,
     constraint item_pk
         primary key (item_id)
 )
